f2f0cd07ca194c91a4b9b4f6adee9cb3d87478a7
Brandon Ashley <tb@tba.sh>
2018-03-25 14:29:10 -0400

    chore(*): update item list decoder

--- src/Data/Item.elm
+++ src/Data/Item.elm
@@ -17,13 +17,21 @@ decoder =
         toLink =
             (++) "https://www.instagram.com/p/"
     in
-    Decode.map4 Item
-        (Decode.at [ "likes", "count" ] Decode.int)
-        (Decode.field "thumbnail_src" Decode.string)
-        (Decode.field "code" Decode.string |> Decode.map toLink)
-        (Decode.field "caption" Decode.string)
+    Decode.field "node" <|
+        Decode.map4 Item
+            (Decode.at [ "edge_liked_by", "count" ] Decode.int)
+            (Decode.field "thumbnail_src" Decode.string)
+            (Decode.field "shortcode" Decode.string |> Decode.map toLink)
+            (Decode.at [ "edge_media_to_caption", "edges" ] <|
+                Decode.map (Maybe.withDefault "" << List.head) <|
+                    Decode.list <|
+                        Decode.map identity <|
+                            Decode.at [ "node", "text" ] Decode.string
+            )
 
 
 listDecoder : Decoder (List Item)
 listDecoder =
-    Decode.at [ "user", "media", "nodes" ] (Decode.list decoder)
+    Decode.list decoder
+        |> Decode.at
+            [ "graphql", "user", "edge_owner_to_timeline_media", "edges" ]
